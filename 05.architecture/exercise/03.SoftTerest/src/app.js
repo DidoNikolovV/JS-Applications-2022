6f28da9a0ce6ac472aa6bbb64fabfb441f186b3d
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-03 08:46:50 +0300

    Add logout functionality

--- 05.architecture/exercise/03.SoftTerest/src/app.js
+++ 05.architecture/exercise/03.SoftTerest/src/app.js
@@ -1,12 +1,13 @@
 // import * as api from './api/users.js';
 
+import { initialize } from './router.js';
+import { logout } from './api/users.js';
 import { showHome } from './views/home.js';
 import { showLogin } from './views/login.js';
 import { showCatalog } from './views/catalog.js';
 import { showRegister } from './views/register.js';
 import { showDetails } from './views/details.js';
 import { showCreate } from './views/create.js';
-import { initialize } from './router.js';
 
 document.getElementById('views').remove();
 
@@ -16,7 +17,8 @@ const links = {
     '/login': showLogin,
     '/register': showRegister,
     '/details': showDetails,
-    '/create': showCreate
+    '/create': showCreate,
+    '/logout': onLogout
 };
 
 const router = initialize(links);
@@ -25,5 +27,10 @@ router.updateNav();
 // Start application in home view
 router.goTo('/');
 
+function onLogout() {
+    logout();
+    router.updateNav();
+    router.goTo('/');
+}
 
 
