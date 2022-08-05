416ca6467de9c09109b904e329ad8d9b9f410158
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-05 09:28:43 +0300

    Render users

--- 06.client-side-rendering/demo-custom-lit-html/src/app.js
+++ 06.client-side-rendering/demo-custom-lit-html/src/app.js
@@ -1,14 +1,25 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
 
-const template = () => html`
+const template = (items, styleClass, isDisabled) => html`
     <h1>Hello from Lit Html</h1>
-    <ul>
-        <li>First</li>
-        <li>Second</li>
-        <li>Third</li>
+    
+    ${items.length == 0
+       ? html`<p>No users</p>`
+       : ''
+    }
+    
+    <ul class=${styleClass}>
+        ${items.map(x => html`<li>${x}</li>`)}
     </ul>
+    <button ?disabled=${isDisabled} @click=${() => onClick()}>Add</button>
 `;
 
+const names = ['Pesho', 'Gosho', 'Stamat', 'Mariyka'];
 const root = document.getElementById('root');
 
-render(template(), root);
+render(template(['Pesho', 'Gosho'], 'vertical', false), root);
+
+function onClick() {
+    names.push('Pesho - ' + Math.random());
+    render(template(names, 'vertical', false), root);
+}
