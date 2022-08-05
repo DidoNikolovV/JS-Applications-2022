8faeefffadf28a6c4886d773f9df759e12196885
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-05 09:34:47 +0300

    Disabled button when li elements are more than 6

--- 06.client-side-rendering/demo-custom-lit-html/src/demo.js
+++ 06.client-side-rendering/demo-custom-lit-html/src/demo.js
@@ -1,6 +1,6 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
 
-const template = (items, styleClass, isDisabled) => html`
+const template = (items, styleClass) => html`
     <h1>Hello from Lit Html</h1>
     
     ${items.length == 0
@@ -11,15 +11,15 @@ const template = (items, styleClass, isDisabled) => html`
     <ul class=${styleClass}>
         ${items.map(x => html`<li>${x}</li>`)}
     </ul>
-    <button ?disabled=${isDisabled} @click=${() => onClick()}>Add</button>
+    <button ?disabled=${items.length > 6} @click=${() => onClick()}>Add</button>
 `;
 
-const names = ['Pesho', 'Gosho', 'Stamat', 'Mariyka'];
+const names = ['Pesho', 'Gosho', 'Stamat'];
 const root = document.getElementById('root');
 
-render(template(['Pesho', 'Gosho'], 'vertical', false), root);
+render(template(['Pesho', 'Gosho'], 'vertical'), root);
 
 function onClick() {
     names.push('Pesho - ' + Math.random());
-    render(template(names, 'vertical', false), root);
+    render(template(names, 'vertical'), root);
 }
