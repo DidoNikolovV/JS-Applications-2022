39e5dcbc989ec7aa6edeebe26b62ea413ae45b30
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-06 09:21:51 +0300

    Show & hide status code

--- 06.client-side-rendering/exercise/02.HTTP-Status-Cats/app.js
+++ 06.client-side-rendering/exercise/02.HTTP-Status-Cats/app.js
@@ -1,16 +1,18 @@
 import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { styleMap } from '../node_modules/lit-html/directives/style-map.js';
 import { cats } from './catSeeder.js';
 
 const root = document.getElementById('allCats');
+root.addEventListener('click', showStatus);
 
-const cardPreview = (data, showStatus) => html`
+const cardPreview = (data) => html`
 <ul>
     ${data.map(cat => html`
     <li>
         <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
         <div class="info">
-            <button class="showBtn" @click=${showStatus}>Show status code</button>
-            <div class="status" style="display: none" id="${cat.id}">
+            <button class="showBtn">${cat.info ? 'Hide' : 'Show'} status code</button>
+            <div class="status" style=${styleMap(cat.info ? { display: 'block'  } : { display: 'none'  })} id="${cat.id}">
                 <h4>Status Code: ${cat.statusCode}</h4>
                 <p>${cat.statusMessage}</p>
             </div>
@@ -19,11 +21,15 @@ const cardPreview = (data, showStatus) => html`
     `)}
 </ul>
 `
+
+cats.forEach(c => c.info = false);
 update();
 
 function showStatus(e) {
-    const elementId = e.target.parentNode;
-    console.log(elementId);
+    const elementId = e.target.parentNode.querySelector('.status').id;
+    const cat = cats.find(c => c.id == elementId);
+    cat.info = !cat.info;
+    update();
 }
 
 function update() {
