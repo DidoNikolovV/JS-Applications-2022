83768274d67f083833bef87ef4cea798bc49a6be
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-08 11:57:22 +0300

    Configure routing with placeholder modules

---
+++ 07.routing/01.Furniture/src/app.js
@@ -0,0 +1,27 @@
+import { page, render } from './lib.js';
+import { catalogPage } from './views/catalog.js';
+import { registerPage } from './views/register.js';
+import { loginPage } from './views/login.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+
+
+const root = document.querySelector('.container')
+
+page(decorateContext);
+page('/', catalogPage);
+page('/details/:id', detailsPage);
+page('/register', registerPage);
+page('/login', loginPage);
+page('/create', createPage);
+page('/edit/:id', editPage);
+page('/my-furniture', catalogPage);
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root)
+
+    next();
+}
