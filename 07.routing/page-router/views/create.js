c2c802a6a1f4c19648fed7cb2edd6772da9ad7f5
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-08 10:01:07 +0300

    Change htmlFor to for

--- 07.routing/page-router/views/create.js
+++ 07.routing/page-router/views/create.js
@@ -24,12 +24,12 @@ const createArticleHandler = async (ctx, e) => {
 const createTemplate = (ctx) => html`
     <form @submit=${createArticleHandler.bind({}, ctx)}>
         <div>
-            <label htmlFor="title">Title</label>
+            <label for="title">Title</label>
             <input type="text" id="title" name="title" />
         </div>
     
         <div>
-            <label htmlFor="content">Content</label>
+            <label for="content">Content</label>
             <textarea name="content" id="" cols="30" rows="10"></textarea>
         </div>
     
@@ -38,7 +38,7 @@ const createTemplate = (ctx) => html`
         </div>
     
         <div>
-            <label htmlFor="author">Author</label>
+            <label for="author">Author</label>
             <input type="text" id="author" name="author" />
         </div>
     
