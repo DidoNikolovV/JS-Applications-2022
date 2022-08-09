ea8a7e17e031685e22941ee742f26c4e30f81dcb
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-09 10:51:32 +0300

    Add login and register methods in api

--- 07.routing/01.Furniture/src/api/api.js
+++ 07.routing/01.Furniture/src/api/api.js
@@ -1,45 +1,93 @@
+import { clearUserData, getUserData, setUserData } from "../util.js";
+
 const host = 'http://localhost:3030';
 
-async function request(method, url, data) {
-    const options = {
-        method,
-        headers: {},
-    };
-
-    if (data !== undefined) {
-        options.headers['Content-Type'] = 'application/json';
-        options.body = JSON.stringify(data);
-    }
-
-    const userData = JSON.parse(sessionStorage.getItem('userData'));
-
-    if (userData != null) {
-        options.headers['X-Authorization'] = userData.accessToken;
-    }
-
+async function request(url, options) {
     try {
-        const res = await fetch(host + url, options);
+        const response = await fetch(host + url, options);
 
-        if (res.ok == false) {
-            const error = await res.json();
-            throw Error(error.message);
+        if (response.ok == false) {
+            if (response.status == 403) {
+                clearUserData();
+            }
+            const error = await response.json();
+            throw new Error(error.message);
         }
 
-        if (res.status == 204) {
-            return res;
+        if (response.status == 204) {
+            return response;
         } else {
-            return await res.json();
+            return await response.json();
         }
     } catch (err) {
         alert(err.message);
         throw err;
     }
 }
+function createOptions(method = 'get', data) {
+    const options = {
+        method,
+        headers: {}
+    }
+
+    if (data != undefined) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+
+    const userData = getUserData();
+
+    if (userData) {
+        options.headers['X-Authorization'] = userData.token;
+    }
+
+    return options;
+}
 
 export async function get(url) {
-    return request('get', url);
+    return request(url, createOptions());
 }
 
 export async function post(url, data) {
-    return request('post', url, data);
+    return request(url, createOptions('post', data));
+}
+
+export async function put(url, data) {
+    return request(url, createOptions('put', data));
+}
+
+export async function del(url) {
+    return request(url, createOptions('delete'));
+}
+
+export async function login(email, password) {
+    const result = await post('/users/login', { email, password });
+
+    const userData = {
+        email: result.email,
+        password: result.password,
+        id: result._id,
+        token: result.accessToken
+    };
+
+    setUserData(userData);
+}
+
+export async function register(email, password) {
+    const result = await post('/users/register', { email, password });
+
+    const userData = {
+        email: result.email,
+        password: result.password,
+        id: result._id,
+        token: result.accessToken
+    }
+
+    setUserData(userData);
+    return result;
+}
+
+export async function logout() {
+    await get('/users/logout');
+    clearUserData();
 }
