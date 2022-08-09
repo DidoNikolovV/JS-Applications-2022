ef37862d4bed7a9420e94f3553c136164f6ceb5a
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-09 11:46:45 +0300

    Add error handling in register form

--- 07.routing/01.Furniture/src/views/register.js
+++ 07.routing/01.Furniture/src/views/register.js
@@ -1,26 +1,33 @@
-import { html } from '../lib.js';
 
-const registerTemplate = () => html`
+import { html } from '../lib.js';
+import { register } from '../api/data.js';
+
+
+const registerTemplate = (onSubmit, errorMsg, errors) => html`
     <div class="row space-top">
         <div class="col-md-12">
             <h1>Register New User</h1>
             <p>Please fill all fields.</p>
         </div>
     </div>
-    <form>
+    <form @submit=${onSubmit}>
         <div class="row space-top">
             <div class="col-md-4">
+                ${errorMsg ? html` <div class="form-group error">${errorMsg}</div>` : null}
                 <div class="form-group">
                     <label class="form-control-label" for="email">Email</label>
-                    <input class="form-control" id="email" type="text" name="email">
+                    <input class=${`form-control` + (errors.email ? ' is-invalid' : '')} id="email" type="text"
+                        name="email">
                 </div>
                 <div class="form-group">
                     <label class="form-control-label" for="password">Password</label>
-                    <input class="form-control" id="password" type="password" name="password">
+                    <input class=${"form-control" + (errors.password ? ' is-invalid' : '')} id="password" type="password"
+                        name="password">
                 </div>
                 <div class="form-group">
                     <label class="form-control-label" for="rePass">Repeat</label>
-                    <input class="form-control" id="rePass" type="password" name="rePass">
+                    <input class=${"form-control" + (errors.rePass ? ' is-invalid' : '')} id="rePass" type="password"
+                        name="rePass">
                 </div>
                 <input type="submit" class="btn btn-primary" value="Register" />
             </div>
@@ -29,5 +36,49 @@ const registerTemplate = () => html`
 `
 
 export function registerPage(ctx) {
-    ctx.render(registerTemplate());
+    update(null, {});
+
+    function update(errorMsg, errors) {
+        ctx.render(registerTemplate(onSubmit, errorMsg, errors));
+    }
+
+    async function onSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        const email = formData.get("email").trim();
+        const password = formData.get("password").trim();
+        const rePass = formData.get("rePass").trim();
+
+
+        try {
+            if (email == '' || password == '' || rePass == '') {
+                throw {
+                    error: new Error('All fields are required!'),
+                    errors: {
+                        email: email == '',
+                        password: password == '',
+                        rePass: rePass == ''
+                    }
+                };
+            }
+
+            if (password != rePass) {
+                throw {
+                    error: new Error('Passwords don\'t match!'),
+                    errors: {
+                        password: true,
+                        rePass: true
+                    }
+                };
+            }
+
+            await register(email, password);
+            ctx.updateUserNav();
+            ctx.page.redirect('/');
+        } catch (err) {
+            const message = err.message || err.error.message;
+            update(message, err.errors || {});
+        }
+    }
 }
