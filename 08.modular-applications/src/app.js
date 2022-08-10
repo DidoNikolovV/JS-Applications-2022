89dd1fc096228f97fe3b6cbb87632c1f5b878fe1
Diyan Nikolov <53466577+DidoNikolovV@users.noreply.github.com>
2022-08-10 07:04:57 +0300

    Add Details view for each movie

--- 08.modular-applications/src/app.js
+++ 08.modular-applications/src/app.js
@@ -5,6 +5,7 @@ import { renderMiddleware } from './middlewares/renderMiddleware.js';
 import { authMiddleware } from './middlewares/authMiddleware.js';
 import { logoutView } from './views/logoutView.js';
 import { registerView } from './views/registerView.js';
+import { movieView } from './views/movieView.js';
 
 // Attach middlewares
 page(authMiddleware);
@@ -16,5 +17,6 @@ page('/', homeView);
 page('/login', loginView);
 page('/logout', logoutView);
 page('/register', registerView);
+page('/movies/:movieId', movieView)
 
 page.start();
--- 08.modular-applications/src/services/movieService.js
+++ 08.modular-applications/src/services/movieService.js
@@ -3,3 +3,7 @@ const baseUrl = 'http://localhost:3030/data/movies';
 export const getAll = () => {
     return fetch(baseUrl).then(res => res.json());
 };
+
+export const getOne = (movieId) => {
+    return fetch(`${baseUrl}/${movieId}`).then(res => res.json());
+};
